2fba6a61eb335d9d3ab306b15fc6e5ab710c4ece
saekamupell <saekamupell@gmail.com>
2020-04-03 06:15:28 +0700

    Update description.js

--- repo/description.js
+++ repo/description.js
@@ -84,7 +84,7 @@ function loadRecentUpdates() {
 			for (var dicNow in decodeResp) {
 				var urlOpen = "cydia://package/"+decodeResp[dicNow].package;
 				if (navigator.userAgent.search(/Cydia/) == -1) {
-					urlOpen = window.location.protocol+"//"+window.location.hostname+"/description.html?id="+decodeResp[dicNow].package;
+					urlOpen = window.location.protocol+"//"+window.location.hostname+"repo/description.html?id="+decodeResp[dicNow].package;
 				}
 				htmlnews +=  "<li><a href='"+urlOpen+"' target='_blank'><img class='icon' src='tweak.png'/><label>"+decodeResp[dicNow].name+" v"+decodeResp[dicNow].version+"</label></a></li>";
 			}
